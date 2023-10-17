7662ceaad9fe226dad2be377ac392cce63913298
unknown <salmasulthana466@gmail.com>
2023-10-18 00:27:21 +0530

    commited changes

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,12 +1,14 @@
-import React, {useState,useEffect}from 'react';
-import './index.css';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
 
 const Categories = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchCategory = async () => {
-      const response = await fetch('https://fakestoreapi.com/products/categories');
+      const response = await fetch(
+        "https://fakestoreapi.com/products/categories"
+      );
       const data = await response.json();
       setCategories(data);
     };
@@ -14,19 +16,15 @@ const Categories = () => {
   }, []);
 
   return (
-    <div className='category'>
-   {categories.length===0 && <div>Loading....</div>}
-    {
-      categories.map((category)=>(
-        <Link to={`/categories/${category}`} className='category-card'>
-            <button className='category-btn'>{category.toUpperCase()}</button>
-          </Link>
-      )
-       )
-    } 
-   
+    <div className="category">
+      {categories.length === 0 && <div>Loading....</div>}
+      {categories.map((category) => (
+        <Link to={`/categories/${category}`} className="category-card">
+          <button className="category-btn">{category.toUpperCase()}</button>
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './index.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const Navbar = () => {
   return (
     <header style={{ width: "100%" }}>
       <nav className="header-part">
-        <h3 className='logo'>FLIP MART</h3>
+        <h3 className="logo">FLIP MART</h3>
         <ul className="nav-items">
-          
-          <li ><Link to='/' className='list-items'>Home</Link> </li>
-          <li ><Link to='/products' className='list-items'>Products</Link> </li>
-          <li ><Link to='/about' className='list-items'>Abou us</Link></li>
-          <li ><Link to='/contact' className='list-items'>Contact</Link></li>
-          {/* <li className='list-items'><Link to='/cart'>Cart</Link></li> */}
-          
+          <li>
+            <Link to="/" className="list-items">
+              Home
+            </Link>{" "}
+          </li>
+          <li>
+            <Link to="/products" className="list-items">
+              Products
+            </Link>{" "}
+          </li>
+          <li>
+            <Link to="/about" className="list-items">
+              Abou us
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="list-items">
+              Contact
+            </Link>
+          </li>
         </ul>
         <h5>
-        <Link to='/cart' className='list-items'>Cart</Link>
+          <Link to="/cart" className="list-items">
+            Cart
+          </Link>
         </h5>
-       
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
